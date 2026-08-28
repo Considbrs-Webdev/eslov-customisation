309fe4681a56ff57a7676fb54d0c4df93bb1ab17
Viktor Schultz <viktor.schultz@consid.se>
2026-08-28 14:41:50 +0200

    reduce size in related posts heading

--- views/partials/schema/event/related-posts.blade.php
+++ views/partials/schema/event/related-posts.blade.php
@@ -8,8 +8,14 @@
         'u-padding__y--4@lg',
         'u-padding__x--4@xl',
         'u-padding__y--4@xl'
-    ]])
-    @typography([ 'element' => 'h2', 'variant' => 'h2', 'classList' => ['u-margin__top--0'], 'attributeList' => ['style' => 'color: var(--color-secondary-contrasting);'] ])
+    ]
+])
+    @typography([
+        'element' => 'h2',
+        'variant' => 'h4',
+        'classList' => ['u-margin__top--0'],
+        'attributeList' => ['style' => 'color: var(--color-secondary-contrasting);']
+    ])
         {!! $lang->relatedEventsTitle !!}
     @endtypography
     @include('posts-list', $postsListData)
